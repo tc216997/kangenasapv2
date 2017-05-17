3720b74d7a91cf4d1b84a69b976a735a2981cc87
Terry Chong <tc216997@gmail.com>
2017-05-18 01:15:06 +0800

    should be finished modal

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,3 +1,32 @@
-$(function() {
-  $('#finance-modal').modal('show');
+$(function(){
+  console.log('loaded')
+  $('.button-3d').each(function(){
+    $(this).unbind().click(function(){
+      let val = $(this).val();
+      let name = $(this).attr('name');
+      changeModalTitle();
+      $('.dropdown').click(function(){
+        $('.payment-options').unbind().click(function(){
+          let file = val + '-' + $(this).attr('data-payment');
+          removeEmail();
+          showDownloadDiv();
+          console.log('/pdf?filename=' +  file)
+          $('#machine-pdf').attr('href', '/pdf?filename=' +  file)
+        });
+      });
+    });
+  });
 });
+
+function changeModalTitle() {
+  let title = 'Please choose your payment options for the '  + name;
+  $('.modal-title').text(title);
+}
+
+function removeEmail() {
+  $('#modal-contact').remove();
+}
+
+function showDownloadDiv() {
+  $('#download-div').slideDown('slow');
+}
