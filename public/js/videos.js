a59c4ed0c79e4d036851a4ea32c598b930e248cf
Terry Chong <tc216997@gmail.com>
2017-05-24 05:58:41 +0800

    testing out preloading images

--- public/js/videos.js
+++ public/js/videos.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
   cycleDiv(images[0], images[1], images[2]);
 });
 
-
 function cycleDiv(item1, item2, item3) {
   $(item1).fadeIn(1500).delay(7000).fadeOut(1500, function(){
     $(item2).fadeIn(1500).delay(7000).fadeOut(1500, function(){
@@ -13,3 +12,8 @@ function cycleDiv(item1, item2, item3) {
     });
   });
 }
+
+//preloading images
+(new Image()).src = '../img/science.jpg'; 
+(new Image()).src = '../img/testimonial.jpg';
+(new Image()).src = '../img/demo.jpg';
