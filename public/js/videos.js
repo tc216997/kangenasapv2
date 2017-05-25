220d734c8b3891b9ed4dfbf558818fb0eb1f409f
Terry Chong <tc216997@gmail.com>
2017-05-26 06:10:49 +0800

    merged js files

--- public/js/videos.js
+++ public/js/videos.js
@@ -23,10 +23,25 @@ let images = [],
               '4XVrzoCgGVk'],
     features = ['kiPo8G-Dsb0','6-zywkEqbtM','lh76nCh3kfs'],
     temp = [];
+    youtube = document.querySelectorAll('.youtube');
     //preload images
     (new Image()).src = '../img/science.jpg'; 
     (new Image()).src = '../img/testimonial.jpg';
     (new Image()).src = '../img/demo.jpg';
+
+// click handler for mobile vids
+for (let i = 0; i < youtube.length; i++) {
+  youtube[i].addEventListener('click', function(e){
+    e.preventDefault();
+    let oldEmbed = $('.iframe-container').attr('data-video');
+    let embed = this.getElementsByTagName('img')[0].dataset.embed;
+    let source = 'https://www.youtube.com/embed/' + embed + '?rel=0&showinfo=1';
+    let imgSrc = 'https://img.youtube.com/vi/' + oldEmbed + '/hqdefault.jpg';
+    $('iframe').attr('src', source);
+    $('.iframe-container').attr('data-video', embed);
+    this.getElementsByTagName('img')[0].src = imgSrc;
+  });
+}
 
 
 $(document).ready(function(){
